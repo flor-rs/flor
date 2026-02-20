80c7ca62b2108d9d31dc9e0808de9743ce0aefbf
Misaka_15535 <46892084+Misaka299@users.noreply.github.com>
2026-02-20 23:48:09 +0800

    chore(error): add backtrace printing to error output
    - Captured call stack during error instantiation.
    - Formatted backtrace output in error display for better debugging.

--- crates/flor/src/log_error.rs
+++ crates/flor/src/log_error.rs
@@ -1,4 +1,5 @@
 use log::{error, warn};
+use std::backtrace::Backtrace;
 
 // 1. Trait 名字改为 ResultLogExt (Result Log Extension)，这是 Rust 扩展 Trait 的标准命名惯例
 pub trait ResultLogExt<E> {
@@ -10,17 +11,14 @@ pub trait ResultLogExt<E> {
 }
 
 impl<T, E: std::fmt::Debug> ResultLogExt<E> for Result<T, E> {
-    // 加上 #[track_caller] 是个好习惯，这样日志里显示的文件行号
-    // 会是调用这行代码的地方，而不是这个 Trait 实现的地方。
-    #[track_caller]
     fn error_on_err(self, msg: impl AsRef<str>) {
         if let Err(err) = self.as_ref() {
+            let bt = Backtrace::capture();
             // 这里 msg 放在前面，err 放在后面，阅读体验更好： "Load Config Failed: File not found"
-            error!("{}: {:?}", msg.as_ref(), err);
+            error!("{}: {:?} \n{}", msg.as_ref(), err, bt);
         }
     }
 
-    #[track_caller]
     fn warn_on_err(self, msg: impl AsRef<str>) {
         if let Err(err) = self.as_ref() {
             // 修正：这里应该用 warn! 宏
@@ -29,16 +27,15 @@ impl<T, E: std::fmt::Debug> ResultLogExt<E> for Result<T, E> {
     }
 
     // --- 新增链式实现 ---
-    #[track_caller]
     fn log_err(self, msg: impl AsRef<str>) -> Self {
         if let Err(err) = &self {
+            let bt = Backtrace::capture();
             // 使用 &self 借用打印，不消耗所有权
-            error!("{}: {:?}", msg.as_ref(), err);
+            error!("{}: {:?}\n{}", msg.as_ref(), err, bt);
         }
         self // 原样返回
     }
 
-    #[track_caller]
     fn log_warn(self, msg: impl AsRef<str>) -> Self {
         if let Err(err) = &self {
             warn!("{}: {:?}", msg.as_ref(), err);
